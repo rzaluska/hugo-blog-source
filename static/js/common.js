8b762e7e760d15a91a6bfb844926bcb7319002d6
Radek <Radek@writeingo.com>
2017-07-23 20:27:10 +0200

    Simplify layout

--- static/js/common.js
+++ static/js/common.js
@@ -1,26 +1,2 @@
-function scrollButtons() {
-        if (window.innerWidth > document.documentElement.clientWidth) {
-            $("#go-to-top").click(function(event){
-                event.preventDefault();
-                $("html, body").animate({"scrollTop": "0px"}, "slow");
-            })
-            $("#go-to-top").show("slow");
-
-            $("#go-to-bottom").click(function(event){
-                event.preventDefault();
-                $("html, body").animate({"scrollTop": $(document).height()}, "slow");
-            })
-            $("#go-to-bottom").show("slow");
-        }
-    else {
-            $("#go-to-top").hide('slow');
-            $("#go-to-bottom").hide('slow');
-    }
-}
-
 $(function() {
-    $( window ).resize(function() {
-        scrollButtons();
-    })
-    scrollButtons();
 });
