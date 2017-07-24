ca607b4bd1efa8f3a300106007441cbd799ea319
Radek <Radek@writeingo.com>
2017-07-24 11:42:07 +0200

    Responsive

--- static/js/common.js
+++ static/js/common.js
@@ -1,2 +1,22 @@
 $(function() {
+    (function($) {
+
+        var ost = 0;
+
+        var s = 5;
+
+        $(window).scroll(function() {
+            var cOst = $(this).scrollTop();
+
+            if((cOst - ost) > s && cOst > ost) {
+                $('#hide-on-scroll').addClass('fixed').removeClass('default');
+            }
+            else if ((ost - cOst) > s) {
+                $('#hide-on-scroll').addClass('default').removeClass('fixed');
+            }
+
+            ost = cOst;
+        });
+
+    })(jQuery);
 });
